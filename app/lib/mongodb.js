c7a6b3c99d0049c67900e64f8ee6d88581420ba6
DarthMuzammil <muzammil.inamdar98@gmail.com>
2025-06-11 15:44:49 +0530

    chore: add mongodb URI for prod

--- app/lib/mongodb.js
+++ app/lib/mongodb.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = 'mongodb://localhost:27017/'; //Your mongodb url goes here
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/';
 
 let cached = global.mongoose;
 
